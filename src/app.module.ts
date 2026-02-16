1581d06d32555d30a74315f9d745d717a2c3ed28
VuVanNghia <vuvannghia.work@gmail.com>
2026-02-16 18:18:44 +0700

    feat: configure application with PostgreSQL and improve testing setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,65 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule, ErrorHandlerFilter } from '@toeichust/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { TargetModule } from './modules/target/target.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+
+          url: configService.get(
+            'MICROSERVICES_SETTINGS_COMMANDS_SERVICE_DATABASE_URL',
+          ),
+
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+
+          logging:
+            configService.get<string>('NODE_ENV') === 'development'
+              ? true
+              : false,
+
+          // synchronize:
+          //   configService.get<string>('NODE_ENV') === 'production'
+          //     ? false
+          //     : true,
+          synchronize: true,
+
+          ssl: configService.get<string>('NODE_ENV') === 'test' ? false : true,
+          extra: {
+            ssl:
+              configService.get<string>('NODE_ENV') === 'test'
+                ? false
+                : { rejectUnauthorized: false },
+            max: 1,
+            connectionTimeoutMillis: 5000,
+          },
+        };
+      },
+    }),
+
+    // TargetModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ErrorHandlerFilter,
+    },
+  ],
 })
 export class AppModule {}
